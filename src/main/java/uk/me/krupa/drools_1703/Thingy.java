dec8deacf3723a0fcf903d9404fec87c03b50532
Gerard Krupa <GerardJohn.Krupa@homeoffice.gsi.gov.uk>
2017-08-21 12:31:31 +0100

    Removing 2nd constructor to remove method ambiguity

--- src/main/java/uk/me/krupa/drools_1703/Thingy.java
+++ src/main/java/uk/me/krupa/drools_1703/Thingy.java
@@ -8,22 +8,16 @@ public class Thingy implements Serializable {
     private String version;
     private Object[] items;
 
-    public Thingy(String name) {
-        this.name = name;
-        this.version = null;
-        this.items = null;
-    }
-
-    public Thingy(String name, Object... items) {
-        this.name = name;
-        this.version = null;
-        this.items = items;
-    }
-
     public Thingy(String name, String version, Object... items) {
         this.name = name;
         this.version = version;
         this.items = items;
+    }
+
+    public Thingy(String name) {
+        this.name = name;
+        this.version = null;
+        this.items = null;
     }
 
     public void print() {
